d2ec86eb0a05be66ff1fc4e6810ed570ebf72e11
Mariella Rossi <maruellafotodi@gmail.com>
2025-12-11 14:15:35 +0100

    Fix image file extensions in istruzioni.js

--- istruzioni.js
+++ istruzioni.js
@@ -1,8 +1,8 @@
 if(document.getElementById('vinile1')!=null){
     const vinile1JS = new Image();
-    vinile1JS.src="immagini/vinile1.jpg";
+    vinile1JS.src="immagini/vinile1.JPG";
     const vinile1retro = new Image();
-    vinile1retro.src="immagini/retro1.jpg";
+    vinile1retro.src="immagini/retro1.JPG";
 document.getElementById('vinile1').addEventListener("mouseover",function(){document.getElementById('img1').src=vinile1retro.src})
 document.getElementById('vinile1').addEventListener("mouseout",function(){document.getElementById('img1').src=vinile1JS.src})
 }
@@ -13,3 +13,4 @@ document.getElementById('confermaOrdine').addEventListener("click",function(){wi
 document.getElementById('fuoriprovincia').addEventListener("click",function(){spedizione=10;document.getElementById('totaleHTML').value=totale(),verifica()})
 document.getElementById('provincia').addEventListener("click",function(){spedizione=0;document.getElementById('totaleHTML').value=totale(),verifica()})
 }
+
